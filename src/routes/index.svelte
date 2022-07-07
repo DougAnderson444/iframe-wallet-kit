b2e8be0d229b837c75c0b1953a8b5c2c69b1df98
Doug A <douganderson444@gmail.com>
2022-07-07 16:44:32 -0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import GetKeys from '$lib/GetKeys.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<GetKeys />
+</section>
