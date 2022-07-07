ef95398a08160da4e3afc8483071fb729ab8613e
Doug A <douganderson444@gmail.com>
2022-07-07 16:49:33 -0300

    gh pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,16 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		prerender: {
+			default: true // creates index.html on build folder
+		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/iframe-wallet-kit' : ''
+		},
 
 		// Override http methods in the Todo forms
 		methodOverride: {
